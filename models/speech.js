946696cfac9a8410e0170e4ab0cf4b49de455ce7
RaulCote <raulcote@hotmail.com>
2018-11-08 15:14:02 +0100

    Models done: user & speech.

---
+++ models/speech.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
+const speechSchema = new Schema({
+  owner: { type: ObjectId, ref: 'User' },
+  title: String,
+  message: String,
+  tag: [{type: String}],
+  private: Boolean,
+  },{
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    },
+});
+
+const Speech = mongoose.model('Speech', speechSchema);
+
+module.exports = Speech;
