53734adbf4bbc83c5b1564c211132e259e2a2bca
Esaú JC <esau.jc@gmail.com>
2018-11-10 23:00:50 +0100

    modifie speech

--- routes/speech.js
+++ routes/speech.js
@@ -3,28 +3,74 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const User = require('../models/user');
 const Speech = require('../models/speech');
+const session = require('express-session');
 const { isLoggedIn } = require('../helpers/middlewares');
 
 const ObjectId = mongoose.Types.ObjectId;
 
-router.get('/', (req, res, next) => {
-  // const { id } = req.body;
-  console.log('entra');
+router.get('/:id', (req, res, next) => {
+   const id = req.params.id;
+ 
 
-  Speech.find({}, (err, speechList) => {
+  Speech.findById(id, (err, speech) => {
+    if (err) {
+      next(err);
+    } else {
+      res.status(200).json(speech)
+    }
+  });
+    
+});
+
+// router.get('/profile/myspeeches', (req, res, next) => {
+  
+//   Speech.find({}, (err, speechList) => {
+//     if (err) {
+//       next(err);
+//     } else {
+//       res.status(200).json(speechList)
+//     }
+//   });
+// });
+
+
+router.get('/', (req, res, next) => {
+console.log(req.query);
+  
+ 
+  
+  Speech.find(req.query, (err, speechList) => {
     if (err) {
       next(err);
     } else {
       res.status(200).json(speechList)
     }
   });
+
+  // Speech.find({is_Public: true}, (err, speechList) => {
+  //   if (err) {
+  //     next(err);
+  //   } else {
+  //     res.status(200).json(speechList)
+  //   }
+  // });
+
+
+  // Speech.find({}, (err, speechList) => {
+  //   if (err) {
+  //     next(err);
+  //   } else {
+  //     res.status(200).json(speechList)
+  //   }
+  // });
     
 });
 
 router.post('/', (req, res, next) => {
   // const userId = req.session.currentUser._id;
 
-  console.log('entra');
+  // console.log('entra');
+  // console.log(req.query);
 
   // const owner = ObjectId("5be42047b88f1e220904717a");
   const newSpeech = new Speech({
@@ -68,9 +114,8 @@ router.post('/', (req, res, next) => {
 router.put('/:id', (req,res,next) => {
   const id = req.params.id;
 
-  // let addTag = []
-  // addTag.push(req.body.tag);
-  console.log(req.body.tag);
+  let addTag = []
+  addTag.push(req.body.tag);
 
   const updateSpeech = {
     title: req.body.title,
@@ -88,18 +133,5 @@ router.put('/:id', (req,res,next) => {
   })
 })
 
-router.get('/:id', (req, res, next) => {
-   const id = req.params.id;
- 
-
-  Speech.findById(id, (err, speech) => {
-    if (err) {
-      next(err);
-    } else {
-      res.status(200).json(speech)
-    }
-  });
-    
-});
 
 module.exports = router;
